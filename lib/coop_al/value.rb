94aa89015204ad6a212db813e864b30f8eb9ac1e
Jamie Hale <jamie@smallarmyofnerds.com>
2017-03-10 16:29:53 -0500

    Fix module name and bump version

--- lib/coop_al/value.rb
+++ lib/coop_al/value.rb
@@ -109,23 +109,23 @@ end
 #
 class Integer
   def platinum
-    Coop::Value.platinum(to_i)
+    CoopAl::Value.platinum(to_i)
   end
 
   def gold
-    Coop::Value.gold(to_i)
+    CoopAl::Value.gold(to_i)
   end
 
   def silver
-    Coop::Value.silver(to_i)
+    CoopAl::Value.silver(to_i)
   end
 
   def copper
-    Coop::Value.copper(to_i)
+    CoopAl::Value.copper(to_i)
   end
 
   def electrum
-    Coop::Value.electrum(to_i)
+    CoopAl::Value.electrum(to_i)
   end
 
   alias gp gold
@@ -140,23 +140,23 @@ end
 #
 class String
   def platinum
-    Coop::Value.platinum(roll_dice(self))
+    CoopAl::Value.platinum(roll_dice(self))
   end
 
   def gold
-    Coop::Value.gold(roll_dice(self))
+    CoopAl::Value.gold(roll_dice(self))
   end
 
   def silver
-    Coop::Value.silver(roll_dice(self))
+    CoopAl::Value.silver(roll_dice(self))
   end
 
   def copper
-    Coop::Value.copper(roll_dice(self))
+    CoopAl::Value.copper(roll_dice(self))
   end
 
   def electrum
-    Coop::Value.electrum(roll_dice(self))
+    CoopAl::Value.electrum(roll_dice(self))
   end
 
   alias gp gold
